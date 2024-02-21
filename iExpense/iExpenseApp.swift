cdb0666a69ac3a099f95bb6b49e7126c33a816eb
Jamila <j.ruzimetova@gmail.com>
2024-02-21 10:44:50 -0500

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Jamila Ruzimetova on 2/21/24.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
